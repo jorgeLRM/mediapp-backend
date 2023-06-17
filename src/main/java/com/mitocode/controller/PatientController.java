b659e90927680f970d8835e86327368bddb57203
jorgeLRM <jorge.lrm99@gmail.com>
2023-06-16 18:00:12 -0600

    First commit

---
+++ src/main/java/com/mitocode/controller/PatientController.java
@@ -0,0 +1,8 @@
+package com.mitocode.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientController {
+
+}
---
+++ src/main/java/com/mitocode/repository/PatientRepository.java
@@ -0,0 +1,8 @@
+package com.mitocode.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PatientRepository {
+
+}
